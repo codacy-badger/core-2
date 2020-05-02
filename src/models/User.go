8dfd3343add80031ffc7976aed89ac4c678c22f7
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-02 19:11:47 -0400

    Add user model

---
+++ src/models/User.go
@@ -0,0 +1,16 @@
+package models
+
+// User represents a model for a user
+type User struct {
+	ID        string `json:"id,omitempty" bson:"_id,omitempty"`
+	FirstName string `json:"firstName" bson:"firstName"`
+	LastName  string `json:"lastName" bson:"lastName"`
+	Email     string `json:"email" bson:"email"`
+	Password  string `json:"password" bson:"password"`
+	CreatedAt int64  `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+	UpdatedAt int64  `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+}
+
+func (User) GetUser() {
+
+}
