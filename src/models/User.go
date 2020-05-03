ae62ea20d3b609dc40548d02ea08d6b5a533601e
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-02 23:18:59 -0400

    Use gorm model for user

--- src/models/User.go
+++ src/models/User.go
@@ -1,12 +1,10 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 // User represents a model for a user
 type User struct {
-	ID        string `json:"id,omitempty" bson:"_id,omitempty"`
-	FirstName string `json:"firstName" bson:"firstName"`
-	LastName  string `json:"lastName" bson:"lastName"`
-	Email     string `json:"email" bson:"email"`
-	Password  string `json:"password" bson:"password"`
-	CreatedAt int64  `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
-	UpdatedAt int64  `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
+	gorm.Model
+	Email     string `gorm:"unique;not null"`
+	Password  string
 }
