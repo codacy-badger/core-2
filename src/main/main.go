7c4ab4a3d4414d07261e982bb42b41dac4170ea9
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-06 02:04:30 -0400

    Update entry point

--- src/main/main.go
+++ src/main/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/iborg-ai/core/src/database"
-	"github.com/iborg-ai/core/src/router"
 	"github.com/iborg-ai/core/src/server"
 )
 
 func main() {
-	router.RegisterRoutes()
 	database.ConnectDB()
 	database.AutoMigrate()
 	server.CreateServer()
