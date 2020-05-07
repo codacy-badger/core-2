111a443b8254baaaab8a3585f717653151283d5e
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-06 23:34:54 -0400

    Add token validation logic

--- src/middleware/RouteAuthorization.go
+++ src/middleware/RouteAuthorization.go
@@ -1,16 +1,63 @@
 package middleware
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber"
+	"github.com/iborg-ai/core/src/models"
 )
+
+var key = os.Getenv("core_TOKEN_KEY")
 
 // VerifyToken verifies if the token present in the authorization header is valid
 func VerifyToken(c *fiber.Ctx) {
 	token := c.Get("Authorization")
 
 	if token == "" {
-		c.SendStatus(401)
+		c.Status(http.StatusUnauthorized).JSON(&models.HTTPErrorStatus{Status: http.StatusUnauthorized, Message: http.StatusText(http.StatusUnauthorized)})
 		return
 	}
 	c.Next()
+}
+
+// CreateToken returns the token and error after signing with HS256
+func CreateToken(email string) (string, error) {
+	if key == "" {
+		key = "core"
+	}
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &models.Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(key))
+}
+
+// ValidateToken validates a token payload
+func ValidateToken(tokenString string) int {
+	if key == "" {
+		key = "core"
+	}
+	claims := &models.Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(key), nil
+	})
+	if err != nil {
+		log.Println(err)
+		if err == jwt.ErrSignatureInvalid {
+			return http.StatusUnauthorized
+		}
+		return http.StatusBadRequest
+	}
+	if !token.Valid {
+		return http.StatusUnauthorized
+	}
+	return http.StatusOK
 }
