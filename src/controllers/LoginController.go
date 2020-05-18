2352b3b48788ccfea085acf6ff5f88a86471396e
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-17 22:09:15 -0400

    Return proper data

--- src/controllers/LoginController.go
+++ src/controllers/LoginController.go
@@ -1,8 +1,8 @@
 package controllers
 
 import (
-	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber"
 	"github.com/iborg-ai/core/src/middleware"
@@ -12,7 +12,6 @@ import (
 
 // LoginController represents the entry point for the Login API
 func LoginController(c *fiber.Ctx) {
-	log.Println("Login API")
 	middleware.AddHeaders(c)
 
 	switch c.Method() {
@@ -29,22 +28,34 @@ func LoginController(c *fiber.Ctx) {
 }
 
 func doLogin(c *fiber.Ctx) {
-	log.Println("Do Login")
-
 	user := new(models.User)
 	if err := c.BodyParser(user); err != nil {
 		c.SendStatus(http.StatusBadRequest)
 		return
 	}
 
-	result := services.DoLogin(user.Email, user.Password)
-	if result.Status == http.StatusOK {
-		tokenString, err := middleware.CreateToken(user.Email)
+	dbUser, err := services.DoLogin(user.Email, user.Password)
+	if err != nil {
+		c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+			"status": http.StatusUnauthorized,
+			"error":  err.Error(),
+		})
+	} else {
+		tokenString, err := middleware.CreateToken(dbUser.ID, dbUser.Email)
 		if err != nil {
-			c.Status(http.StatusInternalServerError).Send(err)
+			c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"status": http.StatusInternalServerError,
+				"error":  err.Error(),
+			})
 			return
 		}
 		c.Set("Authorization", "Bearer "+tokenString)
+		c.Set("UserID", strconv.FormatUint(uint64(dbUser.ID), 16))
+		c.Set("Email", string(dbUser.Email))
+		c.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "OK",
+			"userID":  dbUser.ID,
+			"email":   dbUser.Email,
+		})
 	}
-	c.Status(result.Status).JSON(result)
 }
