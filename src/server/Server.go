345cb28809075aa6a56c6fdae5f07c47a1331788
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-17 22:06:53 -0400

    Add middleware

--- src/server/Server.go
+++ src/server/Server.go
@@ -3,7 +3,11 @@ package server
 import (
 	"os"
 
+	"github.com/gofiber/compression"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/limiter"
+	"github.com/gofiber/logger"
+	"github.com/gofiber/recover"
 	"github.com/iborg-ai/core/src/router"
 )
 
@@ -14,6 +18,31 @@ func CreateServer() {
 		port = "8080"
 	}
 	app := fiber.New()
+
+	// Use compression
+	app.Use(compression.New())
+
+	// Use recovery from panic
+	recoverConf := recover.Config{
+		Handler: func(c *fiber.Ctx, err error) {
+			c.SendString(err.Error())
+			c.SendStatus(500)
+		},
+	}
+	app.Use(recover.New(recoverConf))
+
+	// Use logger to log HTTP requests
+	app.Use(logger.New())
+
+	// Use API rate limiter
+	limiterConf := limiter.Config{
+		Max:     10,
+		Timeout: 5,
+	}
+	app.Use(limiter.New(limiterConf))
+
+	// Register app routes
 	router.RegisterRoutes(app)
+
 	app.Listen(port)
 }
