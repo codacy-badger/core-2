920e2a4eb73e65d808c222a68e7cdd6d6d1face6
Praveen Ravichandran <me@pravinba9495.dev>
2020-05-17 22:07:12 -0400

    Update user model

--- src/models/User.go
+++ src/models/User.go
@@ -1,10 +1,24 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
 
 // User represents a model for a user
 type User struct {
 	gorm.Model
-	Email    string `gorm:"unique;not null"`
-	Password string
+	Email    string `gorm:"unique_index;not null;default:''"`
+	Password string `gorm:"not null;default:''"`
+}
+
+// Validate method validates a given model
+func (user User) Validate(db *gorm.DB) {
+	if user.Email == "" {
+		db.AddError(errors.New("Email Cannot be empty"))
+	}
+	if user.Password == "" {
+		db.AddError(errors.New("Password cannot be empty"))
+	}
 }
